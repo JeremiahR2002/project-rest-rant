8185bed5252736cc38f6ea80958620ecf917ea6e
Jeremiah Reid <jeremiahdelacruzreid@gmail.com>
2022-08-28 01:26:13 -0400

    Adding and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,8 +5,13 @@ function home () {
     return (
       <Def>
           <main>
-
               <h1>HOME</h1>
+            <div>
+                <img src="/images/Magical-Castle.jpg" alt="Its a castle!" />
+            </div>
+            <div>
+                Photo by <a href="https://unsplash.com/photos/oBmPqzGii6E">Prometheus Design</a> on <a href="https://unsplash.com/s/photos/free">Unsplash</a>
+            </div>
           </main>
           <a href="/places">
             <button className="btn-primary">Places Page</button>
